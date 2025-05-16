fac7e2ac3f9dd7fbf00e258cab7180d8e0942327
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-17 06:02:48 +0700

    refactor: change font and use sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import "../styles/globals.css";
 import type { Metadata } from "next";
+import { Noto_Sans_Thai } from "next/font/google";
+
 import QueryProvider from "@/providers/query-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const noto = Noto_Sans_Thai({
+  subsets: ["thai"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "Warehouse Management System",
@@ -14,8 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className={`${noto.className} antialiased`}>
         <QueryProvider>{children}</QueryProvider>
+        <Toaster />
       </body>
     </html>
   );
