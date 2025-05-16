e8594ea439b9b88fe1fb55c7c876bf5bd0010dfc
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-17 06:00:49 +0700

    chore: add new utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalize(str: string) {
+  if (!str) return "";
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function capitalizeWords(str: string) {
+  return str
+    .split(" ")
+    .map((word) => capitalize(word))
+    .join(" ");
+}
+
+export function generateUniqueId(prefix: string): string {
+  const dateTime = new Date().toLocaleDateString().replace(/\//g, "");
+  const randomSuffix = Math.random().toString(36).substring(2, 4);
+  return `${prefix.toUpperCase()}${dateTime}${randomSuffix.toUpperCase()}`;
 }
