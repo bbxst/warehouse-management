8046dec7228920743ec79456ca9e2dcc5de50f10
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-17 23:01:14 +0700

    build(backend): add swagger, posgres, and basic setup

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,6 @@
 using backend.Data;
+using backend.Services;
+using backend.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddScoped<IInventoryService, InventoryService>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -17,6 +31,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 app.MapControllers();
 
