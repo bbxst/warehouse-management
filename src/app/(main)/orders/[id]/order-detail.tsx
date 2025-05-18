79fb6b5e8bc0bc7192eb9c4a2237535634e948f0
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-19 03:54:18 +0700

    feat(frontend): add order detail page

---
+++ src/app/(main)/orders/[id]/order-detail.tsx
@@ -0,0 +1,292 @@
+"use client";
+
+import { z } from "zod";
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
+import { Trash } from "lucide-react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import { OrderDetail, OrderDetailItem } from "@/types";
+import { apiRequest } from "@/lib/utils";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SubmitButton } from "@/components/submit-button";
+
+const orderItemSchema = z.object({
+  id: z.string().nonempty("Id is required"),
+  quantity: z.number().min(1, "Quantity must be positive"),
+});
+
+const addOrderSchema = z.object({
+  id: z.string().nonempty({ message: "Id is required" }),
+  items: z.array(orderItemSchema).nonempty("At least one item is required"),
+});
+
+type AddOrderFormData = z.infer<typeof addOrderSchema>;
+
+export default function OrderDetailClient({ id }: { id: string }) {
+  const [items, setItems] = useState<OrderDetailItem[]>([]);
+  const [search, setSerch] = useState<string>("");
+  const [total, setTotal] = useState<number>(0);
+
+  const queryClient = useQueryClient();
+
+  const { data: orderDetail, isLoading } = useQuery<OrderDetail>({
+    queryKey: ["orders", id],
+    queryFn: async () => {
+      return await apiRequest<OrderDetail>(`/api/orders/${id}`, "GET");
+    },
+    enabled: !!id,
+  });
+
+  const form = useForm<AddOrderFormData>({
+    resolver: zodResolver(addOrderSchema),
+    defaultValues: {
+      id: id,
+      items: [],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "items",
+  });
+
+  useEffect(() => {
+    if (!!orderDetail) {
+      setItems(orderDetail.items);
+      setTotal(orderDetail.total);
+      const mappedItems = orderDetail.items.map((item) => {
+        return {
+          id: item.id,
+          quantity: item.quantity,
+        };
+      });
+      if (mappedItems.length > 0) {
+        form.setValue(
+          "items",
+          mappedItems as [(typeof mappedItems)[0], ...typeof mappedItems]
+        );
+      }
+    }
+  }, [form, orderDetail]);
+
+  const { data: searchItem } = useQuery<OrderDetailItem[]>({
+    queryKey: ["inventory", search],
+    queryFn: async () => {
+      return await apiRequest<OrderDetailItem[]>(
+        `/api/inventory?status=1&name=${search}`,
+        "GET"
+      );
+    },
+    enabled: search.length > 0,
+  });
+
+  const handleAddItem = (item: OrderDetailItem) => {
+    if (items.some((i) => i.id === item.id)) {
+      toast.error("Item already added to the order.");
+      setSerch("");
+      return;
+    }
+    setItems((prevItems) => [...prevItems, item]);
+    append({ id: item.id, quantity: 1 });
+    setTotal((prevTotal) => prevTotal + item.price * 1);
+    setSerch("");
+  };
+
+  const handleRemoveItem = (index: number) => {
+    setItems((prevItems) => {
+      const newItems = [...prevItems];
+      newItems.splice(index, 1);
+      return newItems;
+    });
+    setTotal(
+      (prevTotal) => prevTotal - items[index].price * fields[index].quantity
+    );
+    remove(index);
+  };
+
+  const mutation = useMutation({
+    mutationFn: async (data: AddOrderFormData) => {
+      await apiRequest<AddOrderFormData>(
+        `/api/orders/${id}/items`,
+        "PUT",
+        data
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["inventory"] });
+      toast.success(`Order created successfully`);
+    },
+    onError: (error) => {
+      toast.error(`Error: ${error.message}`);
+    },
+    onSettled: () => {
+      form.reset();
+      setItems([]);
+      setTotal(0);
+    },
+  });
+
+  const onSubmit = async (data: AddOrderFormData) => {
+    mutation.mutate(data);
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex-1 flex flex-col h-full p-6 gap-6 border rounded-lg overflow-hidden"
+      >
+        <div className="flex-1 flex flex-col gap-6 overflow-hidden">
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl font-semibold">{id}</h1>
+          </div>
+          {/* <div className="relative">
+            <Command>
+              <CommandInput
+                value={search}
+                onValueChange={(search) => setSerch(search)}
+                placeholder="Search item name..."
+              />
+              {search.length > 0 && (
+                <CommandList className="absolute top-9 w-full z-10 bg-white border border-t-0">
+                  <CommandEmpty>No results found.</CommandEmpty>
+                  <CommandGroup>
+                    {searchItem?.map((item) => (
+                      <CommandItem
+                        key={item.id}
+                        onSelect={() => handleAddItem(item)}
+                      >
+                        {item.name}
+                      </CommandItem>
+                    ))}
+                  </CommandGroup>
+                </CommandList>
+              )}
+            </Command>
+          </div> */}
+          <div className="flex-1 flex flex-col overflow-hidden border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Id</TableHead>
+                  <TableHead>Name</TableHead>
+                  <TableHead>Quantity</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead>Action</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {fields.map((item, index) => (
+                  <TableRow key={item.id}>
+                    <TableCell className="font-medium">
+                      {items[index].id}
+                    </TableCell>
+                    <TableCell>{items[index].name}</TableCell>
+                    <TableCell>
+                      <Popover>
+                        <PopoverTrigger className="h-8 px-4 rounded-lg hover:bg-accent">
+                          {form.watch(`items.${index}.quantity`)}
+                        </PopoverTrigger>
+                        <PopoverContent className="flex w-fit gap-2">
+                          <FormField
+                            control={form.control}
+                            name={`items.${index}.quantity`}
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormControl>
+                                  <Input
+                                    {...field}
+                                    className="w-20"
+                                    type="number"
+                                    min={1}
+                                    max={items[index].quantity}
+                                    value={field.value}
+                                    onChange={(e) => {
+                                      const newQnt =
+                                        Number(e.target.value) > 0
+                                          ? Number(e.target.value)
+                                          : 1;
+                                      const price = items[index].price;
+                                      setTotal((prevTotal) => {
+                                        const oldValue = field.value * price;
+                                        const newTotal =
+                                          prevTotal - oldValue + price * newQnt;
+                                        return newTotal;
+                                      });
+                                      field.onChange(Number(newQnt));
+                                    }}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                        </PopoverContent>
+                      </Popover>
+                    </TableCell>
+                    <TableCell>{items[index].price}</TableCell>
+                    <TableCell>
+                      <Button
+                        onClick={() => handleRemoveItem(index)}
+                        className="size-10"
+                        variant="ghost"
+                      >
+                        <Trash />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+        <div className="text-xl font-semibold px-3">
+          <span>Total</span>
+          <span className="float-right text-xl font-semibold">
+            ฿ {total.toLocaleString()}
+          </span>
+        </div>
+        {/* <SubmitButton
+          isValid={form.formState.isValid}
+          state={mutation.isPending}
+          text="Place Order"
+        /> */}
+      </form>
+    </Form>
+  );
+}
--- src/app/(main)/orders/[id]/page.tsx
+++ src/app/(main)/orders/[id]/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import OrderDetail from "./order-detail";
 
-export default function page() {
-  return <div></div>;
+export default async function OrderDetailPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return <OrderDetail id={id} />;
 }
