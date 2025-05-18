e11807e3a98efd01cd627a5b7041632095276912
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-18 21:29:01 +0700

    refactor(backend): improve inventory

--- backend/Interfaces/IInventoryService.cs
+++ backend/Interfaces/IInventoryService.cs
@@ -1,13 +1,13 @@
-using System;
 using backend.Models;
+using backend.ViewModels;
 
 namespace backend.Interfaces;
 
 public interface IInventoryService
 {
-  Task<IEnumerable<Item>> GetItems(string? name);
+  Task<IEnumerable<ItemViewModel>> GetItems(string? name);
   Task<Item?> GetItem(string id);
-  Task<string> AddItem(Item item);
-  Task<string> UpdateItem(Item item);
-  Task<string> DeleteItem(string id);
+  Task<ItemViewModel> AddItem(AddOrEditItemViewModel item);
+  Task<ItemViewModel> UpdateItem(string id, AddOrEditItemViewModel item);
+  Task<bool> DeleteItem(string id);
 }
