ea448cec414ce4586422f125bdea9748175dd6ac
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-18 21:27:14 +0700

    refactor(backend): update view models

--- backend/ViewModels/OrderViewModel.cs
+++ backend/ViewModels/OrderViewModel.cs
@@ -1,24 +1,42 @@
 ﻿using backend.Models;
-using System.Collections.ObjectModel;
 
 namespace backend.ViewModels
 {
+    public class OrderDetail
+    {
+        public required string Id { get; set; }
+        public OrderType Type { get; set; }
+        public OrderStatus Status { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public required List<OrderItemDetail> Items { get; set; }
+    }
+
+    public class OrderItemDetail
+    {
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+    }
+
     public class AddOrderDto
     {
         public required OrderType Type { get; set; }
         public required List<OrderItemDto> Items { get; set; }
     }
 
-    public class UpdateOrderStatus
-    {
-        public required string Id { get; set; }
-        public OrderStatus Status { get; set; }
-    }
-
     public class OrderItemDto
     {
         public required string Id { get; set; } 
         public decimal Quantity { get; set; }
+    }
+
+    public class UpdateOrderStatus
+    {
+        public required string Id { get; set; }
+        public OrderStatus Status { get; set; }
     }
 
     public class UpdateMultipleOrderItemsDto
