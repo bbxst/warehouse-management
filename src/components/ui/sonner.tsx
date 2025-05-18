2360310d2758964d77ce5b7a562aaaca92fedfb3
Thanat Onkasepol <91747619+bbxst@users.noreply.github.com>
2025-05-19 00:22:17 +0700

    refactor(frontend): sonner show on top when on mobile

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import { useIsMobile } from "@/hooks/use-mobile";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "light" } = useTheme();
+  const isMobile = useIsMobile();
 
   return (
     <Sonner
       richColors
+      position={isMobile ? "top-center" : "bottom-right"}
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
       style={
